02dda908770b7e05c4a5332d844c033fd3fbe50e
Abhijit47 <akarmakar846@gmail.com>
2026-07-21 09:02:35 +0530

    Add contact-us route with components for pending, error, and not found states

--- src/routes/contact-us.tsx
+++ src/routes/contact-us.tsx
@@ -1,9 +1,58 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { Spinner } from '#/components/ui/spinner'
+import { createFileRoute, useRouter } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/contact-us')({
   component: RouteComponent,
+  pendingComponent: PendingComponent,
+  errorComponent: ErrorComponent,
+  notFoundComponent: NotFoundComponent,
+  wrapInSuspense: true,
+  codeSplitGroupings: [
+    ['component', 'pendingComponent', 'errorComponent', 'notFoundComponent'],
+  ],
 })
 
 function RouteComponent() {
-  return <div>Hello "/contact"!</div>
+  return (
+    <main className={'mx-auto max-w-(--breakpoint-xl) space-y-8 px-4'}>
+      <div className={'h-48 w-full text-center'}>
+        <h1 className={'text-2xl font-semibold'}>
+          Under construction. Please check back later.
+        </h1>
+      </div>
+    </main>
+  )
+}
+
+function PendingComponent() {
+  return (
+    <div
+      className={
+        'mx-auto max-w-(--breakpoint-lg) flex flex-col items-center justify-center h-dvh'
+      }
+    >
+      <Spinner className={'size-6'} />
+    </div>
+  )
+}
+
+function NotFoundComponent() {
+  return (
+    <div className={'mx-auto max-w-(--breakpoint-lg) space-y-8 px-4 py-12'}>
+      <h1 className={'text-2xl font-semibold'}>Page not found</h1>
+    </div>
+  )
+}
+
+function ErrorComponent({ error }: { error: Error }) {
+  const router = useRouter()
+  return (
+    <div className="error">
+      <h2>Something went wrong!!!</h2>
+      <p>{error.message}</p>
+      <button onClick={() => router.navigate({ to: '/tests', search: {} })}>
+        Reset Search
+      </button>
+    </div>
+  )
 }
